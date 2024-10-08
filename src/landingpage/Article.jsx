c399cf0c43c9dcb267a2474c89bce8d14bf6baf3
JHOSEPH <jhoseph@localhost.localdomain>
2024-10-08 15:42:40 -0400

    change: landing page

--- src/landingpage/Article.jsx
+++ src/landingpage/Article.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Article = () => {
   return (
     <div>
-      <section className="dark:bg-gray-100 ">
+      <section className=" ">
         <div className="container flex flex-col justify-center items-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
           <div className="flex flex-col justify-center  p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
             <h1 className="text-4xl font-bold leading-none lg:text-4xl xl:text-7xl">
